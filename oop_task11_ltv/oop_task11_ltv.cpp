f10b6d3bd60cc916700371218eacb4b36e51ccc6
RLworkAcc <roman.litvinenko.salt@gmail.com>
2025-11-27 19:30:34 +0500

    Add files via upload

---
+++ oop_task11_ltv/oop_task11_ltv.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+const int INF = 1e9;
+
+// флойда
+void floydWarshall(vector<vector<int>>& dist) {
+    int n = dist.size();
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
+
+// мейн
+int main() {
+    int n = 4;
+    vector<vector<int>> dist = {
+        {0, 5, INF, 10},
+        {INF, 0, 3, INF},
+        {INF, INF, 0, 1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(dist);
+
+    cout << "Кратчайшие пути:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF) cout << "INF ";
+            else cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
